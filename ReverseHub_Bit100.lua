684a4167e19d571992c10318165434abcecf2453
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2024-12-31 00:22:47 +0700

    Update ReverseHub_Bit100.lua

--- ReverseHub_Bit100.lua
+++ ReverseHub_Bit100.lua
@@ -6,5 +6,5 @@ game.StarterGui: SetCore ("SendNotification", {
   })
 wait(5)
 If game.PlaceId 16732694052 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/THEBOSSBK/Reverse_Hub/refs/heads/main/ReverseHub_Bit100.lua"))()
+    loadstring(game:HttpGet("https://github.com/THEBOSSBK/Reverse_Hub/raw/refs/heads/main/ReverseHub_Bit100_Fisch.lua"))()
 end
