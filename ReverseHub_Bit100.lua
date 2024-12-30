d1af998c05fedbee7eb10904af69198e6150bf73
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2024-12-30 22:02:21 +0700

    Update ReverseHub_Bit100.lua

--- ReverseHub_Bit100.lua
+++ ReverseHub_Bit100.lua
@@ -1,1 +1,7 @@
-test
+game.StarterGui: SetCore ("SendNotification", {
+    Title = "Reverse_Hud";
+    Text = "Thank you for using";
+    Icon = "http://www.roblox.com/asset/?id=12316561489";
+    Duration = "7";
+  })
+wait(5)
