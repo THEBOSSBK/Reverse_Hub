ab1cae092526669ac554b4407bbcb90df22da4e2
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2024-12-31 13:15:40 +0700

    Update ReverseHub_Bit100.lua

--- ReverseHub_Bit100.lua
+++ ReverseHub_Bit100.lua
@@ -12,7 +12,7 @@ local Window = Rayfield:CreateWindow({
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+      FileName = "ReverseHub"
    },
 
    Discord = {
@@ -23,9 +23,9 @@ local Window = Rayfield:CreateWindow({
 
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
+      Title = "ReverseHub",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      Note = "The key is 'Admin1' ", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
