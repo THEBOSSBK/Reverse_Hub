8c5b27849a0d9fea953376ed79414b6d0797a69c
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2025-01-01 20:07:00 +0700

    Update ReverseHub_checking.lua

--- ReverseHub_checking.lua
+++ ReverseHub_checking.lua
@@ -2,4 +2,7 @@
 local fisch = loadstring(game:HttpGet("https://raw.githubusercontent.com/THEBOSSBK/Reverse_Hub/refs/heads/main/ReverseHub_Bit100.lua"))()
 
 if game.PlaceId = 16732694052 then
-  Callback = fisch
+  fisch()
+else
+    print("PlaceId ไม่ตรงกัน")
+  end
