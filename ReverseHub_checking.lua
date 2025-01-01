64803248fdfa4fa0e46b0efb54c2630f9d554207
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2025-01-01 19:54:52 +0700

    Create ReverseHub_checking.lua

---
+++ ReverseHub_checking.lua
@@ -0,0 +1,5 @@
+--map--
+local fisch = loadstring(game:HttpGet("https://raw.githubusercontent.com/THEBOSSBK/Reverse_Hub/refs/heads/main/ReverseHub_Bit100.lua"))()
+
+If game.PlaceId = 16732694052 then
+  fisch
