bae5cf7557dbee09a3e9cbda676a84d4e64f95c0
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2025-01-02 12:24:10 +0700

    Update ReverseHub_checking.lua

--- ReverseHub_checking.lua
+++ ReverseHub_checking.lua
@@ -1,8 +1,16 @@
+local function notify(title, message)
+    -- ใช้ระบบแจ้งเตือนพื้นฐาน
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = title;  -- ชื่อหัวข้อ
+        Text = message; -- ข้อความ
+        Duration = 5;   -- ระยะเวลาการแจ้งเตือน (วินาที)
+    })
+end
 --map--
 local fisch
 
 if game.PlaceId == 16732694052 then
     fisch = loadstring(game:HttpGet("https://raw.githubusercontent.com/THEBOSSBK/Reverse_Hub/refs/heads/main/ReverseHub_Bit100.lua"))()
 else
-    print("PlaceId ไม่ตรงกัน")
+    notify("ไม่พบแมพในระบบ", "ต้องการโหลดแบบทั่วไปไหม?")
 end
