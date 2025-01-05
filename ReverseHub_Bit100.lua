369925db108575f3e9c0b3545758a238a56bb79e
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2025-01-05 09:35:11 +0700

    Update ReverseHub_Bit100.lua

--- ReverseHub_Bit100.lua
+++ ReverseHub_Bit100.lua
@@ -78,7 +78,7 @@ local Toggle = Tab:CreateToggle({
             spawn(function()
                 pcall(function()
                     while _G.AutoFishing do
-                        task.wait(1)
+                        task.wait(1.5)
                         local Rod = Char:FindFirstChildOfClass("Tool") -- ค้นหาเบ็ดตกปลา
                         if Rod and Rod:FindFirstChild("events") then
                             Rod.events.cast:FireServer(100, 1) -- ใช้งาน cast
