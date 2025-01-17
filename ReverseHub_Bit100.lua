38b81f5036d209fdffaa33f34eea09fee52ca7eb
THEBOSSBK <84185714+THEBOSSBK@users.noreply.github.com>
2025-01-17 19:27:18 +0700

    Update ReverseHub_Bit100.lua

--- ReverseHub_Bit100.lua
+++ ReverseHub_Bit100.lua
@@ -154,6 +154,11 @@ local Toggle3 = Tab:CreateToggle({
    --หน้าที่2
 local Tab1 = Window:CreateTab("ผู้เล่น", "user-2")
 local Label = Tab1:CreateLabel("Remove Oxygen", "droplets")
+
+
+
+local Tab2 = Window:CreateTab("ผู้เล่น", "user-2")
+local Label = Tab2:CreateLabel("Remove Oxygen", "droplets")
 local ToggleOxygen = Tab1:CreateToggle({
     Name = "Remove Oxygen",
     CurrentValue = false,
